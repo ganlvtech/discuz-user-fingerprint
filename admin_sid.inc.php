fad9b5372ec7378ab7df3986da8479485340dd8a
Ganlv <ganlvtech@qq.com>
2018-11-29 05:45:34 +0800

    Add access denied to admin php

--- admin_sid.inc.php
+++ admin_sid.inc.php
@@ -2,6 +2,10 @@
 
 namespace Ganlv\UserFingerprint;
 
+if (!defined('IN_DISCUZ') || !defined('IN_ADMINCP')) {
+    exit('Access Denied');
+}
+
 require_once __DIR__ . '/function/function_admin_template.php';
 
 admin_render('sid');
