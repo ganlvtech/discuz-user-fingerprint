b18f908d20e2486c00089c40d88267427acd9d26
Ganlv <ganlvtech@qq.com>
2019-03-06 19:32:41 +0800

    js no surrending function scope

--- js/src/index.js
+++ js/src/index.js
@@ -3,35 +3,34 @@
  * Copyright (C) 2018 Ganlv <https://github.com/ganlvtech>
  * License: GPL 3.0
  */
-!function () {
-    var Fingerprint2 = require('fingerprintjs2');
 
-    function send(murmur, murmur2) {
-        var scriptElement = document.createElement('script');
-        scriptElement.src = '/plugin.php?id=user_fingerprint&fingerprint=' + murmur + '&fingerprint2=' + murmur2;
-        document.getElementsByTagName('head')[0].appendChild(scriptElement);
-    }
+var Fingerprint2 = require('fingerprintjs2');
 
-    function fingerprintReport() {
-        Fingerprint2.get(function (components) {
-            var murmur = Fingerprint2.x64hash128(components.map(function (pair) {
-                return pair.value;
-            }).join(), 31);
-            var murmur2 = Fingerprint2.x64hash128(components.map(function (pair) {
-                if (pair.key === 'canvas' || pair.key === 'webgl' || pair.key === 'webglVendorAndRenderer') {
-                    return '';
-                }
-                return pair.value;
-            }).join(), 31);
-            send(murmur, murmur2);
-        });
-    }
+function send(murmur, murmur2) {
+  var scriptElement = document.createElement('script');
+  scriptElement.src = '/plugin.php?id=user_fingerprint&fingerprint=' + murmur + '&fingerprint2=' + murmur2;
+  document.getElementsByTagName('head')[0].appendChild(scriptElement);
+}
 
-    if (window.requestIdleCallback) {
-        requestIdleCallback(function () {
-            setTimeout(fingerprintReport, 1000);
-        });
-    } else {
-        setTimeout(fingerprintReport, 3000);
-    }
-}();
+function fingerprintReport() {
+  Fingerprint2.get(function (components) {
+    var murmur = Fingerprint2.x64hash128(components.map(function (pair) {
+      return pair.value;
+    }).join(), 31);
+    var murmur2 = Fingerprint2.x64hash128(components.map(function (pair) {
+      if (pair.key === 'canvas' || pair.key === 'webgl' || pair.key === 'webglVendorAndRenderer') {
+        return '';
+      }
+      return pair.value;
+    }).join(), 31);
+    send(murmur, murmur2);
+  });
+}
+
+if (window.requestIdleCallback) {
+  requestIdleCallback(function () {
+    setTimeout(fingerprintReport, 1000);
+  });
+} else {
+  setTimeout(fingerprintReport, 3000);
+}
