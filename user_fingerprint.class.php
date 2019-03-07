887c91207dcc7eba0eb50df535de460eacfad7aa
Coxxs <Coxxs@users.noreply.github.com>
2019-03-07 11:19:56 +0800

    fix tab

--- user_fingerprint.class.php
+++ user_fingerprint.class.php
@@ -28,7 +28,7 @@ class plugin_user_fingerprint
             dsetcookie($name, random(6), $expire, !$keep);
         }
 
-		if (!config('js_path')) return;
+        if (!config('js_path')) return;
         $js_path = config('js_path', 'source/plugin/user_fingerprint/js/dist/index.min.js');
         return '<script src="' . $js_path . '" async defer></script>';
     }
